f597068f7b7940194d623dcd6a11d37c16d66069
Dawid Szarwark <daq131@gmail.com>
2025-01-04 15:57:53 -0400

    Add some methods, rebuild the function

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-def palindrome(text):
-    '''
-    A function that takes a string as an argument and checks if it is a palindrome 
-    '''
-    if text == text[::-1]:
-        return True
-    else:
-        return False
+# def palindrome(text):
+#     '''
+#     A function that takes a string as an argument and checks if it is a palindrome 
+#     '''
+#     def palindrome(text):
+#         test = True if text == text[::-1] else False
+#         print(test)
+        
+# palindrome('kajak')
 
-print(palindrome('kajak'))
+
+def palindrome_sentence(txt):
+    new_text = txt.replace(' ', '').replace(',','').replace('.', '').lower()
+    return True if new_text == new_text[::-1] else False
+    
+palindrome_sentence('a, To kanapa pana kota')
+palindrome_sentence('a tu mam mamuta')
+palindrome_sentence('Wczora z wieczora')
